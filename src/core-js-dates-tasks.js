bc5e833fa5638aafc17417abb895ba1b16823384
Aleksei Arkhangelskii <arhmost@gmail.com>
2024-03-25 18:27:55 +0100

    feat/solve 8

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -183,8 +183,17 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const date = new Date(year, month, 0);
+  const daysInMonth = date.getDate();
+  let count = 0;
+  for (let i = 1; i <= daysInMonth; i += 1) {
+    const day = new Date(year, month - 1, i).getDay();
+    if (day === 0 || day === 6) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
